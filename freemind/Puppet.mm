8bcfe071e1ded21c38f3008c5683e8967dfb17cd
Scot P. floess <sfloess@nc.rr.com>
2010-11-07 12:31:11 -0500

    Work in progress:  Adding mind mapping for now

---
+++ freemind/Puppet.mm
@@ -0,0 +1,20 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1288452963071" ID="ID_61106087" LINK="FlossWare.mm" MODIFIED="1289150959148" TEXT="Puppet">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1288453021755" ID="ID_297507779" MODIFIED="1289150963341" POSITION="right" TEXT="Standards">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1288453025051" ID="ID_290276674" MODIFIED="1289150963343" TEXT="Variables">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1288453029740" ID="ID_1954860208" MODIFIED="1289150963344" TEXT="[class name]_[variable name]">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1288453070524" ID="ID_713746172" MODIFIED="1288453081180" TEXT="dns_defaultListenOn"/>
+</node>
+</node>
+</node>
+</node>
+</map>

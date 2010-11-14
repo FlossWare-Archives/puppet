d726b8763f830dda209b5f9c16b2709998a20f9c
Scot P. floess <sfloess@nc.rr.com>
2010-11-14 12:46:44 -0500

    Work in progress:  Initial checkin of postgres module

---
+++ modules/service/postgres/manifests/init.pp
@@ -0,0 +1,26 @@
+class postgres {
+    package { "postgresql":
+        ensure => installed,
+    }
+}
+
+class postgres::server inherits postgres {
+    package { "postgresql-server":
+        ensure => installed,
+    }
+
+    service { "postgresql":
+        ensure  => running,
+        enable  => true,
+    }
+
+    file { "/var/lib/pgsql/pg_hba.conf":
+        content => template ( "postgres/pg_hba.conf" ),
+        notify  => Service [ "postgresql" ],
+        owner   => "postgres",
+        group   => "postgres",
+    }
+}
+
+class postgres::client inherits postgres {
+}

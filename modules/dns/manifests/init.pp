7dabfc5115b6610708ba1617decd8653523af3f1
Scot P. floess <sfloess@nc.rr.com>
2010-12-10 13:29:10 -0500

    Work in progress:  moving dns over to new variables class structure

--- modules/dns/manifests/init.pp
+++ modules/dns/manifests/init.pp
@@ -1,22 +1,24 @@
-class dns inherits service {
-    $dns_defaultSearch = $defaults::dns_defaultSearch ? {
-        ''      => $service_internalDomain,
-        default => $defaults::dns_defaultSearch,
-    }
+class dns {
+    class variables inherits common:variables {
+        $dns_defaultSearch = $defaults::dns_defaultSearch ? {
+            ''      => $common_internalDomain,
+            default => $defaults::dns_defaultSearch,
+        }
 
-    $dns_defaultNameServer = $defaults::dns_defaultNameServer ? {
-        ''      => $service_masterHost,
-        default => $defaults::dns_defaultNameServer,
-    }
+        $dns_defaultNameServer = $defaults::dns_defaultNameServer ? {
+            ''      => $common_masterHost,
+            default => $defaults::dns_defaultNameServer,
+        }
 
-    $dns_search = $dns_search ? {
-        ''      => $dns_defaultSearch,
-        default => $dns_search
-    }
+        $dns_search = $dns_search ? {
+            ''      => $dns_defaultSearch,
+            default => $dns_search
+        }
 
-    $dns_nameServer = $dns_nameServer ? {
-        ''      => $dns_defaultNameServer,
-        default => $dns_nameServer
+        $dns_nameServer = $dns_nameServer ? {
+            ''      => $dns_defaultNameServer,
+            default => $dns_nameServer
+        }
     }
 }
 
@@ -26,91 +28,93 @@ class dns::client inherits dns {
     }
 }
 
-class dns::server inherits dns {
-    $dns_defaultTTL = $defaults::dns_defaultTTL ? {
-        ''      => "86400",
-        default => $defaults::dns_defaultTTL,
-    }
-
-    $dns_defaultListenOn = $defaults::dns_defaultListenOn ? {
-        ''      => "${service_adminIp};",
-        default => $defaults::dns_defaultListenOn,
-    }
-
-    $dns_defaultForwarders = $defaults::dns_defaultForwarders ? {
-        ''      => "${service_gatewayIp};",
-        default => $defaults::dns_defaultForwarders,
-    }
-
-    $dns_defaultAllowUpdate = $defaults::dns_defaultAllowUpdate ? {
-        ''      => "${dns_defaultListenOn}; 127.0.0.1;",
-        default => $defaults::dns_defaultAllowUpdate,
-    }
-
-    $dns_defaultZones = $defaults::dns_defaultZones ? {
-        ''      => [ "$service_defaultInternalDomain", "${dns_reverseBaseIp}.in-addr.arpa", ],
-        default => $defaults::dns_defaultZones,
-    }
-
-    $dns_defaultDomain = $defaults::dns_defaultDomain ? {
-        ''      => "${service_internalDomain}",
-        default => $defaults::dns_defaultDomain,
-    }
-
-
-    $dns_defaultNetworkNumber = $defaults::dns_defaultNetworkNumber ? {
-        ''      => "${service_networkNumber}",
-        default => $defaults::dns_defaultNetworkNumber,
-    }
-
-    $dns_defaultNameServer = $defaults::dns_defaultNameServer ? {
-        ''      => "${service_masterIp}",
-        default => $defaults::dns_defaultNameServer,
-    }
-
-    $dns_ttl = $dns_ttl ? {
-        ''      => $dns_defaultTTL,
-        default => $dns_ttl,
+class dns::server {
+    class variables inherits dns::variables {
+        $dns_defaultTTL = $defaults::dns_defaultTTL ? {
+            ''      => "86400",
+            default => $defaults::dns_defaultTTL,
         }
 
-    $dns_defaultReverseBaseIp = $dns_defaultReverseBaseIp ? { 
-        ''      => $defaults::reverseBaseIp,
-        default => $dns_defaultReverseBaseIp,
-    }   
-
-    $dns_ListenOn = $dns_listenOn ? {
-        ''      => $dns_defaultListenOn,
-        default => $dns_listenOn,
+        $dns_defaultListenOn = $defaults::dns_defaultListenOn ? {
+            ''      => "${common_adminIp};",
+            default => $defaults::dns_defaultListenOn,
         }
 
-    $dns_forwarders = $dns_forwarders ? {
-        ''      => $dbs_defaultForwarders,
-        default => $dns_forwarders,
+        $dns_defaultForwarders = $defaults::dns_defaultForwarders ? {
+            ''      => "${common_gatewayIp};",
+            default => $defaults::dns_defaultForwarders,
         }
 
-    $dns_allowUpdate = $dns_allowUpdate ? {
-        ''      => $dns_defaultAllowUpdate,
-        default => $dns_allowUpdate,
+        $dns_defaultAllowUpdate = $defaults::dns_defaultAllowUpdate ? {
+            ''      => "${dns_defaultListenOn}; 127.0.0.1;",
+            default => $defaults::dns_defaultAllowUpdate,
         }
 
-    $dns_zones = $dns_zones ? {
-        ''      => $dns_defaultZones,
-        default => $dns_zones
-    }
+        $dns_defaultZones = $defaults::dns_defaultZones ? {
+            ''      => [ "$common_defaultInternalDomain", "${dns_reverseBaseIp}.in-addr.arpa", ],
+            default => $defaults::dns_defaultZones,
+        }
 
-    $dns_domain = $dns_domain ? {
-        ''      => $dns_defaultDomain,
-        default => $dns_domain
-    }
+        $dns_defaultDomain = $defaults::dns_defaultDomain ? {
+            ''      => "${common_internalDomain}",
+            default => $defaults::dns_defaultDomain,
+        }
 
-    $dns_baseIp = $dns_baseIp ? {
-        ''      => $dns_defaultBaseIp,
-        default => $dns_baseIp
-    }
 
-    $dns_hosts = $dns_hosts ? {
-        ''      => $dns_defaultHosts,
-        default => $dns_hosts
+        $dns_defaultNetworkNumber = $defaults::dns_defaultNetworkNumber ? {
+            ''      => "${common_networkNumber}",
+            default => $defaults::dns_defaultNetworkNumber,
+        }
+
+        $dns_defaultNameServer = $defaults::dns_defaultNameServer ? {
+            ''      => "${common_masterIp}",
+            default => $defaults::dns_defaultNameServer,
+        }
+
+        $dns_ttl = $dns_ttl ? {
+            ''      => $dns_defaultTTL,
+            default => $dns_ttl,
+            }
+
+        $dns_defaultReverseBaseIp = $dns_defaultReverseBaseIp ? { 
+            ''      => $defaults::reverseBaseIp,
+            default => $dns_defaultReverseBaseIp,
+        }   
+
+        $dns_ListenOn = $dns_listenOn ? {
+            ''      => $dns_defaultListenOn,
+            default => $dns_listenOn,
+            }
+
+        $dns_forwarders = $dns_forwarders ? {
+            ''      => $dbs_defaultForwarders,
+            default => $dns_forwarders,
+            }
+
+        $dns_allowUpdate = $dns_allowUpdate ? {
+            ''      => $dns_defaultAllowUpdate,
+            default => $dns_allowUpdate,
+            }
+
+        $dns_zones = $dns_zones ? {
+            ''      => $dns_defaultZones,
+            default => $dns_zones
+        }
+
+        $dns_domain = $dns_domain ? {
+            ''      => $dns_defaultDomain,
+            default => $dns_domain
+        }
+
+        $dns_baseIp = $dns_baseIp ? {
+            ''      => $dns_defaultBaseIp,
+            default => $dns_baseIp
+        }
+
+        $dns_hosts = $dns_hosts ? {
+            ''      => $dns_defaultHosts,
+            default => $dns_hosts
+        }
     }
 
     # ------------------------------------------------------
@@ -152,7 +156,7 @@ class dns::server inherits dns {
         notify  => Service [ "named" ],
     }
 
-    zoneFile { $dns_zones: }
+    zoneFile { $variables::dns_zones: }
 
     include dns::client
 }

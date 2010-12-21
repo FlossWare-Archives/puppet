ff16c56d4b0f83e874d1de0a495818322af44d67
Scot P. floess <sfloess@nc.rr.com>
2010-12-20 23:19:09 -0500

    Work in progress:  using the -new- variable functionality

--- modules/ntp/manifests/init.pp
+++ modules/ntp/manifests/init.pp
@@ -1,19 +1,28 @@
-class ntp {
-    class variables inherits common::variables {
-        $ntp_defaultMinPoll = "4"
-        $ntp_defaultMaxPoll = "8"
+include common::variables
 
-        $ntp_minPoll = $ntp_minPoll ? {
-            ''      => $ntp_defaultMinPoll,
-            default => $ntp_minPoll,
-        }
-
-        $ntp_maxPoll = $ntp_maxPoll ? {
-            ''      => $ntp_defaultMaxPoll,
-            default => $ntp_maxPoll,
-        }
+class variables ( $defaultMinPoll = "4", $defaultMaxPoll = "8", $defaultServer, $defaultBroadcast = "" ) inherits common::variables {
+    $ntp_minPoll = $ntp_minPoll ? {
+        ''      => $defaultMinPoll,
+        default => $ntp_minPoll,
     }
 
+    $ntp_maxPoll = $ntp_maxPoll ? {
+        ''      => $defaultMaxPoll,
+        default => $ntp_maxPoll,
+    }
+
+    $ntp_server = $ntp_server ? {
+        ''      => $defaultServer,
+        default => $ntp_server,
+    }
+
+    $ntp_broadcast = $ntp_broadcast ? {
+        ''      => $defaultBroadcast,
+        default => $ntp_broadcast,
+    }
+}
+
+class ntp {
     $packages = [
         "ntp",
         "ntpdate",
@@ -31,20 +40,6 @@ class ntp {
         ensure  => running,
         enable  => true,
     }
-}
-
-class ntp::service {
-    class variables inherits ntp::variables {
-        $ntp_server = $ntp_server ? {
-            ''      => $ntp_defaultServer,
-            default => $ntp_server,
-        }
-
-        $ntp_broadcast = $ntp_broadcast ? {
-            ''      => $ntp_defaultBroadcast,
-            default => $ntp_broadcast,
-        }
-    }
 
     file { "/etc/ntp.conf":
         content => template ( "ntp/client/ntp.conf" ),
@@ -52,15 +47,35 @@ class ntp::service {
     }
 }
 
-class ntp::client inherits ntp {
-    $ntp_defaultServer = "${service_adminIp}"
+class ntp::client::variables {
+    class {
+        variables:
+            defaultServer => "${common::common_masterIp}",
+    }
 
-    include ntp::service
+    $ntp_minPoll   = $variables::ntp_minPoll
+    $ntp_maxPoll   = $variables::ntp_maxPoll
+    $ntp_server    = $variables::ntp_server
+    $ntp_broadcast = $variables::ntp_broadcast
 }
 
-class ntp::server inherits ntp {
-    $ntp_defaultBroadcast = "${service_defaultBroadcastAddress}"
-    $ntp_defaultServer    = "0.north-america.pool.ntp.org"
+class ntp::client inherits ntp::client::variables {
+    include ntp
+}
 
-    include ntp::service
+class ntp::server::variables {
+    class {
+        variables:
+            defaultBroadcast = "${common::common_broadcastAddress}",
+            defaultServer    = "0.north-america.pool.ntp.org",
+    }
+
+    $ntp_minPoll   = $variables::ntp_minPoll
+    $ntp_maxPoll   = $variables::ntp_maxPoll
+    $ntp_server    = $variables::ntp_server
+    $ntp_broadcast = $variables::ntp_broadcast
+}
+
+class ntp::server inherits ntp::server::variables {
+    include ntp
 }
--- modules/postfix/manifests/init.pp
+++ modules/postfix/manifests/init.pp
@@ -1,9 +1,38 @@
-class postfix {
-    class variables inherits common::variables {
-        $postfix_defaultMyhostname = ${common_externalDomain}
-        $postfix_defaultMyorigin   = ${common_externalDomain}
+include common::variables
+
+class variables ( $defaultRelayHost, $defaultInetInterfaces, $defaultMyhostname = $common::variables::common_externalDomain, $defaultMyorigin = $common::variables::common_externalDomain, $defaultProxyInterfaces = "", $defaultMydestination = "" ) inherits common::variables {
+    $postfix_relayHost = $postfix_relayHost ? {
+        ''      => $defaultRelayHost,
+        default => $postfix_relayHost,
     }
 
+    $postfix_inetInterfaces = $postfix_inetInterfaces ? {
+        ''      => $defaultInetInterfaces,
+        default => $postfix_inetInterfaces,
+    }
+
+    $postfix_myhostname = $postfix_myhostname ? {
+        ''      => $defaultMyhostname
+        default => $postfix_myhostname
+    }
+
+    $postfix_myorigin = $postfix_myorigin ? {
+        ''      => $defaultMyorigin}
+        default => $postfix_myorigin
+    }
+
+    $postfix_proxyInterfaces = $postfix_proxyInterfaces ? {
+        ''      => $defaultProxyInterfaces
+        default => $postfix_proxyInterfaces
+    }
+
+    $postfix_mydestination = $postfix_mydestination ? {
+        ''      => $defaultMydestination
+        default => $postfix_mydestination
+    }
+}
+
+class postfix {
     $packages = [
         "postfix",
     ]
@@ -16,40 +45,6 @@ class postfix {
         ensure  => running,
         enable  => true,
     }
-}
-
-class postfix::service {
-    class variables inherits postfix::variables {
-        $postfix_relayHost = $postfix_relayHost ? {
-            ''      => $postfix_defaultRelayHost,
-            default => $postfix_relayHost,
-        }
-
-        $postfix_inetInterfaces = $postfix_inetInterfaces ? {
-            ''      => $postfix_defaultInetInterfaces,
-            default => $postfix_inetInterfaces,
-        }
-
-        $postfix_myhostname = $postfix_myhostname ? {
-            ''      => $postfix_defaultMyhostname
-            default => $postfix_myhostname
-        }
-
-        $postfix_myorigin = $postfix_myorigin ? {
-            ''      => $postfix_defaultMyorigin}
-            default => $postfix_myorigin
-        }
-
-        $postfix_proxyInterfaces = $postfix_proxyInterfaces ? {
-            ''      => $postfix_defaultProxyInterfaces
-            default => $postfix_proxyInterfaces
-        }
-
-        $postfix_mydestination = $postfix_mydestination ? {
-            ''      => $postfix_defaultMydestination
-            default => $postfix_mydestination
-        }
-    }
 
     file { "/etc/postfix/main.cf":
         content => template ( "postfix/main.cf.erb" ),
@@ -57,18 +52,42 @@ class postfix::service {
     }
 }
 
-class postfix::server inherits postfix {
-    $postfix_defaultRelayHost       = "${defaults::externalRelayHost}"
-    $postfix_defaultInetInterfaces  = "${common_masterHost}, ${common_masterFqdn}, localhost, localhost.localdomain"
-    $postfix_defaultProxyInterfaces = $common_masterGatewayIp
-    $postfix_defaultMydestination   = '\$myhostname, \$myhostname.$mydomain, localhost.\$mydomain, localhost, localhost.localdomain, ${common_externalDomain}'
+class postfix::server::variables {
+    class {
+        variables:
+            defaultRelayHost       => "${common::common_externalRelayHost}",
+            defaultInetInterfaces  => "${common::common_masterHost}, ${common::common_masterFqdn}, localhost, localhost.localdomain",
+            defaultProxyInterfaces => $common::common_masterGatewayIp,
+            defaultMydestination   => '\$myhostname, \$myhostname.$mydomain, localhost.\$mydomain, localhost, localhost.localdomain, ${common::common_externalDomain}',
+    }
 
-    include postfix::service
+    $postfix_relayHost       = $variables::postfix_relayHost
+    $postfix_inetInterfaces  = $variables::postfix_inetInterfaces
+    $postfix_myhostname      = $variables::postfix_myhostname
+    $postfix_myorigin        = $variables::postfix_myorigin
+    $postfix_proxyInterfaces = $variables::postfix_proxyInterfaces
+    $postfix_mydestination   = $variables::postfix_mydestination
 }
 
-class postfix::client inherits postfix {
-    $postfix_defaultRelayHost      = "${defaults::internalRelayHost}"
-    $postfix_defaultInetInterfaces = 'all'
+class postfix::server inherits postfix::server::variables {
+    include postfix
+}
 
-    include postfix::service
+class postfix::client::variables {
+    class {
+        variables:
+            defaultRelayHost       => "${common::common_internalRelayHost}",
+            defaultInetInterfaces  => "all",
+    }
+
+    $postfix_relayHost       = $variables::postfix_relayHost
+    $postfix_inetInterfaces  = $variables::postfix_inetInterfaces
+    $postfix_myhostname      = $variables::postfix_myhostname
+    $postfix_myorigin        = $variables::postfix_myorigin
+    $postfix_proxyInterfaces = $variables::postfix_proxyInterfaces
+    $postfix_mydestination   = $variables::postfix_mydestination
+}
+
+class postfix::client inherits postfix::client::variables {
+    include postfix
 }

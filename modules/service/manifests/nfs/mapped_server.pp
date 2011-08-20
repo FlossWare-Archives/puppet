1b62fb3233307babcd063b3c2eadddb8ed119092
Scot P. floess <sfloess@nc.rr.com>
2011-08-20 13:51:48 -0400

    Added puppet documentation for NFS server modules

--- modules/service/manifests/nfs/mapped_server.pp
+++ modules/service/manifests/nfs/mapped_server.pp
@@ -1,3 +1,43 @@
+# This class represents an NFS server, exporting multiple directories.
+#
+# == Parameters
+#
+# [*exportMap*]
+#   A hash that should contain the following keys:  
+#     dir         = the directory to export.
+#     domain      = who is allowed to share dir.  For example *.foo.com
+#     permissions = when exporting dir, the permissions on the share.  For example, rw,no_root_squash
+#
+# == Variables
+#
+# == Examples
+#
+# $home = {
+#     dir         => '/home',
+#     domain      => '*.foo.com',
+#     permissions => 'rw,no_root_squash',
+# }
+#
+# $shared = {
+#     dir         => '/shared',
+#     domain      => '*.foo.com',
+#     permissions => 'rw,no_root_squash',
+# }
+#
+# $exports = {
+#     $home,
+#     $shared,
+# }
+#
+# class {
+#     'service::nfs::mapped_server':
+#         exportMap => $exports,
+# }
+#
+# == Authors
+#
+# Scot P. Floess <flossware@gmail.com>
+#
 class service::nfs::mapped_server ( $exportMap ) inherits service::nfs:base {
     util::enable_service_def {
         'nfs':
--- modules/service/manifests/nfs/server.pp
+++ modules/service/manifests/nfs/server.pp
@@ -1,3 +1,36 @@
+# This class represents an NFS server, exporting one directory.  If you
+# want to export more than one directory, please use service::nfs::mapped_server.
+#
+# == Parameters
+#
+# [*dir*]
+#   The directory to export.
+#
+# [*domain*]
+#   Who is allowed to share dir.  For example *.foo.com
+#
+# [*permissions*]
+#   When exporting dir, the permissions on the share.  For example, rw,no_root_squash
+#
+# == Variables
+#
+# [*exportMap*]
+#   Builds a hash containing dir, domain and permissions.  This variable is then
+#   used and passed to service::nfs::mapped_server
+#
+# == Examples
+#
+# class {
+#    'service::nfs::server':
+#      dir         => '/home', 
+#      domain      => '*.foo.com',
+#      permissions => 'rw,no_root_squash',
+# }
+#
+# == Authors
+#
+# Scot P. Floess <flossware@gmail.com>
+#
 class service::nfs::server ( $dir, $domain, $permissions ) inherits service::nfs:base {
     $exportMap = {
         dir         => $dir,

b944f1585aa663b89d3374f09fddb793b627617d
Scot P. floess <sfloess@nc.rr.com>
2011-10-18 12:54:15 -0400

    Now can handle a sequence of strings

--- modulesTest/tst/manifests/string.pp
+++ modulesTest/tst/manifests/string.pp
@@ -1,5 +1,6 @@
 class tst::string ( $str ) {
+    notice ("Received [0:${str[0]} 1:${str[1]} 2:${str[2]} 3:${str[3]} 4:${str[4]}]")
     tst::string_def {
-        "${str}":
+        $str:
     }
 }

08b09932f2cb402170d98c30dcefe2cf7daa7dfc
Scot P. floess <sfloess@nc.rr.com>
2011-10-29 09:31:54 -0400

    Added puppet doc to NTP

--- modules/service/manifests/ntp.pp
+++ modules/service/manifests/ntp.pp
@@ -1,3 +1,27 @@
+# Class to manage the NTP service.
+#   
+# == Parameters
+#   
+# [*server*]
+#   The server for whom the date/time will be retrieved.
+#   
+# [*minpoll*]
+#   The minimum poll time.
+#   
+# [*maxpoll*]
+#   The maximum poll time.
+#   
+# [*broadcast*]
+#   The broadcast address to use if acting as a server.
+#   
+# == Variables
+#   
+# == Examples
+#   
+# == Authors
+#   
+# Scot P. Floess <flossware@gmail.com>
+#   
 class service::ntp ( $server, $minpoll, $maxpoll, $broadcast = undef ) {
     util::enable_service_def {
         'service::ntp::service':
@@ -5,9 +29,28 @@ class service::ntp ( $server, $minpoll, $maxpoll, $broadcast = undef ) {
             packages => 'ntp',
     }
 
-    service {
-        'ntpdate':
-            enable => true,
+    // Want to enable ntpdate if possible...
+    case $::operatingsystem {
+        CentOS: {
+            case $::lsbmajdistrelease {
+                5: {
+                }
+
+                default: {
+                    service {
+                        'ntpdate':
+                            enable => true,
+                    }
+                }
+            }
+        }
+
+        Fedora: {
+            service {
+                'ntpdate':
+                    enable => true,
+            }
+        }
     }
 
     file {
